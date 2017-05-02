bd3da055daea202ae87be8cdbc4764f160f743db
Benoit Poirier <BenoitPoirier9@outlook.com>
2017-05-02 13:35:02 -0300

    Give annotation

--- ContosoU/Models/Course.cs
+++ ContosoU/Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoU.Models
@@ -12,9 +13,11 @@ namespace ContosoU.Models
         // Computed: Database generates a value when a row is inserted or updated.
         // Identity: Database generates a value when a row is inserted.
         // None: Database generates a value.
+        [Display(Name ="Course Number")]
         public int CourseID { get; set; }//PK
-
+        [StringLength(50,MinimumLength = 3)]
         public string Title { get; set; }
+        [Range(0,5)]
         public int Credits { get; set; }
 
         //================ NAVIGATION PROPERTIES ==================== //
--- ContosoU/Models/Person.cs
+++ ContosoU/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,16 +10,22 @@ namespace ContosoU.Models
     {
         //mwilliams: Create the data models
         public int ID { get; set; }
-
+        [Display(Name ="Last Name")]
+        [Required]
+        [StringLength(65, ErrorMessage = "Last name cannot be longer then 65 characters. ")]
         public string LastName { get; set; }
-
+        [Display(Name = "First Name")]
+        [Required]
+        [StringLength(50,ErrorMessage ="First name cannot be longer then 50 characters. ")]
         public string FirstName { get; set; }
 
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         //FullName: Calculated property with a get accessor only 
         //        - will not get generated in database
         
+            [Display(Name ="Name")]
         public string FullName
         {
             get
